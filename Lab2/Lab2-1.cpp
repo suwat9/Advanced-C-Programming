0c4780afbc8c8f79515e0bf7467a1f8621de1481
Suwat <suwat.tac@mail.pbru.ac.th>
2025-12-09 15:13:17 +0700

    Update Lab2-1.cpp

--- Lab2/Lab2-1.cpp
+++ Lab2/Lab2-1.cpp
@@ -1,1 +1,15 @@
-test
+#include <iostream> // Including the Input/Output Stream Library
+#include <string>   // Including the String Library
+using namespace std;
+
+int main() {
+    char a[6] = {'a', 'b', 'c', 'd', 'e', 'f'}; // Declare and initialize another character array
+    char b[6] = {'A', 'B', 'C', 'D', 'E', 'F'}; // Declare and initialize a character array
+    
+    string mess = "AbbeF"; // Declare and initialize a string variable
+    for (int i = 0; i < mess.length(); i++) {
+        cout << mess[i] << " "; // Output each character in array 'a'
+    }
+    cout << (int)'Z' << endl; // Output the ASCII value of character 'Z'
+    return 0; // Return 0 to indicate successful execution
+}
